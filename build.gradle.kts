b49584275f6cda883e70e6fa7c9b9b5e89929e9c
Cassio Caue <cassio3620@gmail.com>
2024-07-13 12:20:17 -0300

    feat: config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,43 @@
 plugins {
     kotlin("jvm") version "2.0.0"
-
+    kotlin("plugin.spring") version "1.8.20"
+    id("org.springframework.boot") version "2.7.1"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "br.company.vehiclesale"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("org.springdoc:springdoc-openapi-ui:1.5.9")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.5.9")
+
+    runtimeOnly("mysql:mysql-connector-java")
+    implementation("org.flywaydb:flyway-core")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("io.github.microutils:kotlin-logging:2.1.21")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.mockk:mockk:1.12.0")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+
+    implementation("io.kubernetes:client-java:13.0.0")
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
 }
