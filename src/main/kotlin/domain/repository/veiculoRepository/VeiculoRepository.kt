56ca3ae284089fa60343b038bd766985e3c48568
Cassio Caue <cassio3620@gmail.com>
2024-07-13 13:47:43 -0300

    feat: add veiculoRepository, VendaRepository

--- src/main/kotlin/domain/repository/veiculoRepository/VeiculoRepository.kt
+++ src/main/kotlin/domain/repository/veiculoRepository/VeiculoRepository.kt
@@ -1,4 +1,10 @@
 package org.example.domain.repository.veiculoRepository
 
+import org.example.domain.entity.Veiculo.Veiculo
+
 interface VeiculoRepository {
+    fun save(veiculo: Veiculo): Veiculo
+    fun update(veiculo: Veiculo): Veiculo
+    fun findById(id: Long): Veiculo?
+    fun findAllByVendido(vendido: Boolean): List<Veiculo>
 }
